73f0bb282912389f3afd4dbfd62c7a33658ce8da
madjer22 <maciej.nowak.zslg@gmail.com>
2016-09-23 16:41:21 +0200

    List adapter added

---
+++ Exercise4/app/src/main/java/MyListAdapter.java
@@ -0,0 +1,40 @@
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pl.maciej_nowak.exercise4.R;
+
+/**
+ * Created by Maciej on 23.09.2016.
+ */
+public class MyListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    private final Integer[] imgid;
+
+    public MyListAdapter(Activity context, String[] itemname, Integer[] imgid) {
+        super(context, R.layout.item, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.item, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.text);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.image);
+
+        txtTitle.setText(itemname[position]);
+        imageView.setImageResource(imgid[position]);
+        return rowView;
+    };
+}
